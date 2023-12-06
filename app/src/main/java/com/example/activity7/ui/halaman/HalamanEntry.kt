bfac9c2bc4077815bae016c5e3ea38f2c47cba47
overol1221 <fiqiachmad12@gmail.com>
2023-12-06 22:13:21 +0700

    Create object DestinasiEntry

---
+++ app/src/main/java/com/example/activity7/ui/halaman/HalamanEntry.kt
@@ -0,0 +1,11 @@
+package com.example.activity7.ui.halaman
+
+import com.example.activity7.navigasi.DestinasiNavigasi
+import com.example.roomsiswa.R
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
+class HalamanEntry {
+}
