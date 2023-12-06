401cb99095e12e03c29e3511cb6ca2484a746636
overol1221 <fiqiachmad12@gmail.com>
2023-12-06 21:53:31 +0700

    Create DetailSiswa

--- app/src/main/java/com/example/activity7/model/EntryViewModel.kt
+++ app/src/main/java/com/example/activity7/model/EntryViewModel.kt
@@ -2,6 +2,7 @@ package com.example.activity7.model
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.example.activity7.data.Siswa
 import com.example.activity7.repositori.RepositoriSiswa
 
 class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
@@ -34,4 +35,11 @@ data class  DetailSiswa(
     val nama : String= "",
     val alamat : String= "",
     val telpon : String= "",
+)
+
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
 )
