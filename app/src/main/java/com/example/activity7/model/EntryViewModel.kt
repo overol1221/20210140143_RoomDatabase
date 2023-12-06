7d834c155a720eafc2d1885d864eb37378d81ec7
overol1221 <fiqiachmad12@gmail.com>
2023-12-06 21:49:53 +0700

    Create updateUiState

--- app/src/main/java/com/example/activity7/model/EntryViewModel.kt
+++ app/src/main/java/com/example/activity7/model/EntryViewModel.kt
@@ -8,5 +8,13 @@ class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
 
-
+    private fun  validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ): Boolean{
+        return  with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa=
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
 }
