c4df129eaf781e19f51e941b9522b7db1ec8bbee
overol1221 <fiqiachmad12@gmail.com>
2023-12-06 21:48:22 +0700

    Create uiStateSiswa

--- app/src/main/java/com/example/activity7/model/EntryViewModel.kt
+++ app/src/main/java/com/example/activity7/model/EntryViewModel.kt
@@ -1,8 +1,12 @@
 package com.example.activity7.model
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.activity7.repositori.RepositoriSiswa
 
 class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
 
 }
