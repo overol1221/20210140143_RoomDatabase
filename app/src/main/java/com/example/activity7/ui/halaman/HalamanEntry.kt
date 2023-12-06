33ad1855bfd7682ba50e8b5d15318c17c0f0f056
overol1221 <fiqiachmad12@gmail.com>
2023-12-06 22:14:58 +0700

    fun EntrySiswaScreen

--- app/src/main/java/com/example/activity7/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/activity7/ui/halaman/HalamanEntry.kt
@@ -1,5 +1,11 @@
 package com.example.activity7.ui.halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.activity7.model.EntryViewModel
+import com.example.activity7.model.PenyediaViewModel
 import com.example.activity7.navigasi.DestinasiNavigasi
 import com.example.roomsiswa.R
 
@@ -7,5 +13,12 @@ object DestinasiEntry: DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
 class HalamanEntry {
 }
