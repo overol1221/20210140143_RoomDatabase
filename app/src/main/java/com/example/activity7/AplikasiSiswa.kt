5eb33d5bb2347b3dc31d6b53db50e90ee77eed0e
overol1221 <fiqiachmad12@gmail.com>
2023-12-06 22:05:39 +0700

    Create ContainerDataApp

--- app/src/main/java/com/example/activity7/AplikasiSiswa.kt
+++ app/src/main/java/com/example/activity7/AplikasiSiswa.kt
@@ -1,7 +1,14 @@
 package com.example.activity7
 
 import android.app.Application
+import com.example.activity7.repositori.ContainerApp
+import com.example.activity7.repositori.ContainerDataApp
 
 class AplikasiSiswa : Application() {
+    lateinit var container: ContainerApp
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 
 }
