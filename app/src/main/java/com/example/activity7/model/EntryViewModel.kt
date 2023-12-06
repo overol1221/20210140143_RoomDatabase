2f96b9fe193e248fd961e3274c6fcb34e357bb44
overol1221 <fiqiachmad12@gmail.com>
2023-12-06 21:56:39 +0700

    Create fun DetailSiswa atribut

--- app/src/main/java/com/example/activity7/model/EntryViewModel.kt
+++ app/src/main/java/com/example/activity7/model/EntryViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.activity7.model
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.activity7.data.Siswa
 import com.example.activity7.repositori.RepositoriSiswa
@@ -38,6 +40,18 @@ data class  DetailSiswa(
 )
 
 fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
     id = id,
     nama = nama,
     alamat = alamat,
